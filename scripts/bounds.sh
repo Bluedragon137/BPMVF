63f63a47d208836708d8fe69bb3fbb684470bbd2
Alex Lan <alex.lan@gladstone.ucsf.edu>
2021-07-05 01:54:34 -0700

    updates and cleaning before deployment

--- scripts/bounds.sh
+++ scripts/bounds.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-BASE_DIR=/wynton/home/corces/allan/basepairmodels/ENCSR000EGM
+BASE_DIR=/wynton/home/corces/allan/BPMVF/ENCSR000EGM
 DATA_DIR=$BASE_DIR/data
 MODEL_DIR=$BASE_DIR/models
-REFERENCE_DIR=/wynton/home/corces/allan/basepairmodels/reference
+REFERENCE_DIR=/wynton/home/corces/allan/BPMVF/reference
 CHROM_SIZES=$REFERENCE_DIR/hg38.chrom.sizes
 REFERENCE_GENOME=$REFERENCE_DIR/hg38.genome.fa
 CV_SPLITS=$BASE_DIR/splits.json
