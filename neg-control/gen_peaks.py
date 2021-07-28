f47a9db6be10ad0792eafd2e08c023fd642dfa79
Bluedragon137 <alexanderlan137@gmail.com>
2021-07-28 15:04:51 -0700

    revised nc peak generation with distribution

--- neg-control/gen_peaks.py
+++ neg-control/gen_peaks.py
@@ -39,32 +39,43 @@ def save_peaks(origp, neg_sample_list, rate):
     peaks_df = negdf.append(origp)
     peaks_df = peaks_df.sample(frac=1).reset_index(drop=True)
     print(peaks_df.info(), negdf.info(), origp.info())
-    peaks_df.to_csv('data/peaks_df_deduped'+str(rate)+'.bed', sep='\t', encoding='utf-8', header=False, index=False)
+    peaks_df.to_csv('data/peaks_df_deduped'+str(int(rate*100))+'.bed', sep='\t', encoding='utf-8', header=False, index=False)
 
 def gen_peaks_main(origpeaks, negpeaks, rate):
+    #Load original peaks, negative sample datasets
     origp = pd.read_csv(origpeaks, sep='\t', header=None, \
         names=['chrom', 'st', 'en', 'name', 'score', 'strand', 'signalValue', 'p', 'q', 'summit'])
     with open(negpeaks) as f:
         negp = json.load(f)
     fasta_ref = pysam.FastaFile('../reference/hg38.genome.fa')
+
+    #Calculate 
     orig_samples = origp.shape[0]
     neg_samples = math.floor(orig_samples*rate)
-    batchsize = math.floor(1/rate)
     print (orig_samples, neg_samples)
     neg_sample_list = []
-    avg_gc = 0
+    
+    # Get the distribution of GC content in the original dataset 
+    # and scale down to the negative sample quantity
+    distribution = np.zeros([101], dtype=int)
     for index, row in origp.iterrows():
         seq = fasta_ref.fetch(row['chrom'], row['st'], row['en'])
-        avg_gc += get_GC_content(seq)
-        if index % batchsize == batchsize-1:
-            avg_gc = round(avg_gc / batchsize)
-            val, negp = query_gc(avg_gc, negp)
+        gc = round(get_GC_content(seq))
+        distribution[gc-1] += 1
+    distribution = np.multiply(distribution, rate)
+    distribution = distribution.astype(int)
+
+    #Generate the proper number of negative samples
+    for gc in range(101):
+        for i in range(distribution[gc]):
+            val, negp = query_gc(gc, negp)
             neg_sample_list.append(val)
-            avg_gc = 0
-    #print (neg_sample_list, len(neg_sample_list))
-    print ('done processing neg_sample_list')
+    
+    #Save the negative samples to the original dataset and save to bedfile
     save_peaks(origp, neg_sample_list, rate)
 
 if __name__ == '__main__':
-    gen_peaks_main('../ATAC/data/peaks/Cluster24.filtered.deduped.bed', 'data/gc_peaks_dict.json', 0.50)
+    rates = [0.0, 0.05, 0.10, 0.15, 0.20, 0.30, 0.40, 0.50, 0.75, 1.00]
+    for rate in rates:
+        gen_peaks_main('../ATAC/data/peaks/Cluster24.filtered.deduped.bed', 'data/gc_peaks_dict.json', rate)
     
